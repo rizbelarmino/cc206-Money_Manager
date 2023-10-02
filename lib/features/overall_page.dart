282e6537ce61341740de801554e8addb36c9fa2c
rizbelarmino <rizalie.belarmino@wvsu.edu.ph>
2023-10-02 11:13:12 +0800

    branch update

---
+++ lib/features/overall_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class OverallPage extends StatelessWidget{
+  const OverallPage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      children:[
+        Text("Expense Tracker"),
+
+        // TextField(
+        //   decoration: InputDecoration(icon: Icon(Icons.abc))
+        // )
+
+
+
+      ],
+    );
+  }
+}
