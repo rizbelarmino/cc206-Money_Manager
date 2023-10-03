715f39fe7b5371d39697edd09c8e7568a954a98d
ThirdyNeko <ireneo.catequista@wvsu.edu.ph>
2023-10-03 21:10:29 +0800

    update features

---
+++ lib/features/chart.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChartPage extends StatelessWidget {
+  const ChartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("Chart"),
+      ],
+    );
+  }
+}
