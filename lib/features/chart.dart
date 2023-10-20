25e7398585ddc7657870795835eaccaf2da56061
jmabogador <johnmarc.abogador@wvsu.edu.ph>
2023-10-20 20:19:23 +0800

    update

--- lib/features/chart.dart
+++ lib/features/chart.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
 
 class ChartPage extends StatelessWidget {
-  const ChartPage({super.key});
+  const ChartPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("Chart"),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Chart'),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Table(
+            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+            children: [
+              TableRow(
+                children: [
+                  for (int i = 1; i <= 7; i++)
+                    TableCell(
+                      child: Container(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text('Day $i'),
+                      ),
+                    ),
+                  TableCell(
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('Weekly Expenses'),
+                    ),
+                  ),
+                ],
+              ),
+              for (int j = 1; j < 6; j++)
+                TableRow(
+                  children: [
+                    for (int i = 1; i <= 7; i++)
+                      TableCell(
+                        child: Container(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text('Expense Item $j'),
+                        ),
+                      ),
+                    TableCell(
+                      child: Container(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text('Total Expense $j'),
+                      ),
+                    ),
+                  ],
+                ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
