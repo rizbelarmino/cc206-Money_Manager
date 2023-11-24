e5f6d5eaa9a8d2fdfd850550244b096fc22f9ecf
jmabogador <johnmarc.abogador@wvsu.edu.ph>
2023-11-24 19:09:56 +0800

    update

--- lib/features/chart.dart
+++ lib/features/chart.dart
@@ -7,12 +7,27 @@ class ChartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Expense Chart'),
+        title: Text('Expense Chart',
+            style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold)),
+        centerTitle: true, // Center the title
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue, Colors.green],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+          ),
+        ),
       ),
       body: SingleChildScrollView(
         child: Center(
           child: Table(
             defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+            border: TableBorder.all(color: Colors.transparent),
             children: [
               TableRow(
                 children: [
@@ -20,13 +35,39 @@ class ChartPage extends StatelessWidget {
                     TableCell(
                       child: Container(
                         padding: EdgeInsets.all(8.0),
-                        child: Text('Day $i'),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [Colors.grey[300]!, Colors.white],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: Text(
+                          'Day $i',
+                          style:
+                              TextStyle(fontFamily: 'Roboto', fontSize: 16.0),
+                        ),
                       ),
                     ),
                   TableCell(
                     child: Container(
                       padding: EdgeInsets.all(8.0),
-                      child: Text('Weekly Expenses'),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Colors.grey[300]!, Colors.white],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Text(
+                        'Weekly Expenses',
+                        style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold),
+                      ),
                     ),
                   ),
                 ],
@@ -38,13 +79,39 @@ class ChartPage extends StatelessWidget {
                       TableCell(
                         child: Container(
                           padding: EdgeInsets.all(8.0),
-                          child: Text('Expense Item $j'),
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [Colors.white, Colors.grey[200]!],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child: Text(
+                            'Expense Item $j',
+                            style:
+                                TextStyle(fontFamily: 'Roboto', fontSize: 14.0),
+                          ),
                         ),
                       ),
                     TableCell(
                       child: Container(
                         padding: EdgeInsets.all(8.0),
-                        child: Text('Total Expense $j'),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [Colors.white, Colors.grey[200]!],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: Text(
+                          'Total Expense $j',
+                          style: TextStyle(
+                              fontFamily: 'Roboto',
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ),
                   ],
