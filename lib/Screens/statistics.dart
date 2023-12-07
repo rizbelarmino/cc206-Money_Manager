32c0b861e7727477ec120aee08b7e75b4ecce43f
Thirdy <ireneo.catequista@wvsu.edu.ph>
2023-12-07 20:01:52 +0800

    update

---
+++ lib/Screens/statistics.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:managment/data/utlity.dart';
+import 'package:managment/widgets/chart.dart';
+
+import '../data/model/add_date.dart';
+import '../data/top.dart';
+
+class Statistics extends StatefulWidget {
+  const Statistics({Key? key}) : super(key: key);
+
+  @override
+  State<Statistics> createState() => _StatisticsState();
+}
+
+ValueNotifier kj = ValueNotifier(0);
+
+class _StatisticsState extends State<Statistics> {
+  List day = ['Today', 'Week', 'Month', 'Year'];
+  List f = [today(), week(), month(), year()];
+  List<Add_data> a = [];
+  int index_color = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ValueListenableBuilder(
+          valueListenable: kj,
+          builder: (BuildContext context, dynamic value, Widget? child) {
+            a = f[value];
+            return custom();
+          },
+        ),
+      ),
+    );
+  }
+
+  CustomScrollView custom() {
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              Text(
+                'STATISTICS',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 243, 177, 33),
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                  shadows: [
+                    Shadow(
+                      color: Colors.grey,
+                      offset: Offset(2, 2),
+                      blurRadius: 2,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ...List.generate(
+                      4,
+                      (index) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              index_color = index;
+                              kj.value = index;
+                            });
+                          },
+                          child: Container(
+                            height: 40,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color.fromARGB(255, 198, 170, 77),
+                                  Color.fromARGB(255, 243, 177, 33)
+                                ],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                              ),
+                            ),
+                            alignment: Alignment.center,
+                            child: Text(
+                              day[index],
+                              style: TextStyle(
+                                color: index_color == index
+                                    ? Colors.white
+                                    : Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Chart(
+                indexx: index_color,
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Large Expense',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Amount',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        SliverList(
+            delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return ListTile(
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: Image.asset('images/${a[index].name}.png', height: 40),
+              ),
+              title: Text(
+                a[index].name,
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              subtitle: Text(
+                ' ${a[index].datetime.year}-${a[index].datetime.day}-${a[index].datetime.month}',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              trailing: Text(
+                a[index].amount,
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 19,
+                  color: a[index].IN == 'Cash In'
+                      ? Colors.blue
+                      : Colors.deepOrangeAccent,
+                ),
+              ),
+            );
+          },
+          childCount: a.length,
+        ))
+      ],
+    );
+  }
+}
