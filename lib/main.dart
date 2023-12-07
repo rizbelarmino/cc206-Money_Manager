40a1eeec0a8b0f43bf33a9a99144b46f4a4246b5
rizbelarmino <rizalie.belarmino@wvsu.edu.ph>
2023-12-07 10:37:16 +0800

    commit changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'features/overall_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const OverallPage(),
+      home: const ExpenseTracker(),
     );
   }
 }
